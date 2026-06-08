82d688c0ed52d30daa39848e0c3e41fb47c81855
DoritosLover1 <ihsandmrci10@gmail.com>
2026-06-08 21:59:48 +0300

    Fix->About white page in github-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/FinansBilisim-ProjeWebSayfasi/', // GitHub Pages repo ismi
-})
+  // GitHub Pages için build alırken repo ismini kullan, lokalde ise kök dizini (/)
+  base: command === 'build' ? '/FinansBilisim-ProjeWebSayfasi/' : '/', 
+}))
